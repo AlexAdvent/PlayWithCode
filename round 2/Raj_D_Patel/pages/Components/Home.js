0e864b72b3f7986a21be957538eb1ec3e7ab889a
PATEL96 <patel.raj.official.96@gmail.com>
2022-09-15 23:13:47 +0530

    feat: Added My Competetion Code

---
+++ round 2/Raj_D_Patel/pages/Components/Home.js
@@ -0,0 +1,18 @@
+import styles from '../../styles/Home2.module.css'
+
+export default function Home() {
+    return (
+        <div className={styles.Main}>
+            <div className={styles.First}>
+                <div className={styles.Left}>
+                    <div className={styles.Detail1}>CUSTOMIZE YOUR FOOD ACCORDING TO YOUR TASTE</div>
+                    <div className={styles.Detail2}>Custom Food is a user-friendly tool that lets you quick-add specific amounts of carbs, protein, and fat instead of an entire recipe. </div>
+                    <div></div>
+                </div>
+                <div>
+                    Images
+                </div>
+            </div>
+        </div>
+    );
+}
